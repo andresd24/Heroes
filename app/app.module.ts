c037e756afba627c9291cd8353c917a9529b7c9a
andresd24 <andresdev@gmail.com>
2017-01-18 11:45:03 -0500

    update with basic routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,33 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }  from './app.component';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
-  imports:
-      [
+    imports: [
         BrowserModule,
-        FormsModule
-      ],
-  declarations:
-      [
-          AppComponent,
-          HeroDetailComponent
-      ],
-  bootstrap:    [ AppComponent ]
+        FormsModule,
+        RouterModule.forRoot([
+                {
+                    path: 'heroes',
+                    component: HeroesComponent
+                }
+            ])
+        ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
