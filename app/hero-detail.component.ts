7aed62417d529a78357432fd5c97527a701f5eca
andresd24 <andresdev@gmail.com>
2017-01-20 11:16:33 -0500

    adding delete hero and im memory data service

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -2,9 +2,9 @@ import 'rxjs/add/operator/switchMap';
 import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location }               from '@angular/common';
-
 import { Hero }         from './hero';
 import { HeroService }  from './hero.service';
+
 @Component({
     moduleId: module.id,
     selector: 'my-hero-detail',
@@ -29,11 +29,14 @@ export class HeroDetailComponent implements OnInit {
     goBack(): void {
         this.location.back();
     }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
+
+
 }
 
 
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that
- can be found in the LICENSE file at http://angular.io/license
- */
+
